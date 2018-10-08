1da7cc71a2de4bdaa9433806ac1fc4c6757c4eda
Leslie Osorio <lesosorio17@gmail.com>
2018-10-08 21:16:38 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,17 +13,17 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times)
-  while_iterator = 0 
+  while_iterator = 0
 while while_interator < 7
   puts = "Welcome to Flatiron School's Web Development Course is less than 7!"
-while_interator += 1 
+while_interator += 1
 end
 
 def until_iterator(number_of_times)
   until_iterator = 0
   until until_iterator == 7
   puts = "Welcome to Flatiron School's Web Development Course is less than 7!"
-until_iterator += 1 
+until_iterator += 1
 end
 
 def for_iterator(number_of_times)
